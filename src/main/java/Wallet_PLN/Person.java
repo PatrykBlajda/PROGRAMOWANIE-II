f3ed4b83e924c0d07710a6a2e3cc16a077b29d29
Patryk Błajda <patrykblajda@gmail.com>
2020-04-05 00:00:15 +0200

    Person begining...

--- src/main/java/Wallet_PLN/Person.java
+++ src/main/java/Wallet_PLN/Person.java
@@ -1,4 +1,12 @@
 package Wallet_PLN;
 
 public class Person {
+
+    private String Name;
+    private Wallet wallet;
+
+    public Person(String name, Wallet wallet) {
+        Name = name;
+        this.wallet = wallet;
+    }
 }
